766c361b97cae405b383cb5f5bad8921d58fac1a
ponponumi <ponponumi@gmail.com>
2024-11-26 00:09:35 +0900

    変換元のファイルが読み込まれない問題を修正

--- src/ViteLoader.php
+++ src/ViteLoader.php
@@ -109,10 +109,19 @@ class ViteLoader{
 
       switch ($extension) {
         case "css":
+        case "scss":
+        case "sass":
+        case "less":
+        case "stylus":
+        case "styl":
           $html = '<link rel="stylesheet" href="' . $url . '">';
           break;
 
         case "js":
+        case "ts":
+        case "jsx":
+        case "tsx":
+        case "coffee":
           $html = '<script src="' . $url . '"></script>';
           break;
       }
