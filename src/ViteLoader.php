f1322b135d4f11bda961d8d6d0ee25096aafbb63
ponponumi <ponponumi@gmail.com>
2024-11-25 23:54:30 +0900

    Webからのホストを追加するコードを作成

--- src/ViteLoader.php
+++ src/ViteLoader.php
@@ -24,6 +24,7 @@ class ViteLoader{
 
     $this->devMode = array_key_exists('devMode', $viteDevServer) ? boolval($viteDevServer['devMode']) : false;
     $this->devServerHost = array_key_exists('devHost', $viteDevServer) ? $this->lastSlashAdd(strval($viteDevServer['devHost'])) : "";
+    $this->devServerHostWeb = array_key_exists('devHostWeb', $viteDevServer) ? $this->lastSlashAdd(strval($viteDevServer['devHost'])) : $this->devServerHost;
 
     $this->devServerAccessStatus = $this->devServerAccess();
   }
