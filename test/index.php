47758a00bb11f79a829c7f4103907de643b01cbd
ponponumi <ponponumi@gmail.com>
2024-11-26 00:44:43 +0900

    試しに無効化

--- test/index.php
+++ test/index.php
@@ -11,7 +11,7 @@ $vite = new \Ponponumi\ViteLoader\ViteLoader(__DIR__ . "/build/.vite/manifest.js
   "devHostWeb" => $_ENV["VITE_HOST_WEB"] . ":" . $_ENV["VITE_PORT"],
 ]);
 
-$vite->devServerSetting(false);
+// $vite->devServerSetting(false);
 
 ?>
 <!DOCTYPE html>
