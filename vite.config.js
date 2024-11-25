2a0c5ffef8e4c5e0450ab31c2fc2265feeda8bf0
ponponumi <ponponumi@gmail.com>
2024-11-25 23:59:06 +0900

    サーバーの設定を追加

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   build: {
     manifest: true,
     outDir: './test/build',
