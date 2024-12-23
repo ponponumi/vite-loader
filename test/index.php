9b6c817c4600c09ee0b941f8c37fcd012a2a8b83
ponponumi <ponponumi@gmail.com>
2024-12-24 00:42:15 +0900

    インデントを修正

--- test/index.php
+++ test/index.php
@@ -5,10 +5,10 @@ require __DIR__ . "/../vendor/autoload.php";
 $projectRootPath = __DIR__;
 $projectRootUrl = (!empty($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', $projectRootPath);
 
-$vite = new \Ponponumi\ViteLoader\ViteLoader(__DIR__ . "/build/.vite/manifest.json",$projectRootUrl . "/build",false,[
-  "devMode" => true,
-  // "devHost" => $_ENV["VITE_HOST"] . ":" . $_ENV["VITE_PORT"],
-  // "devHostWeb" => $_ENV["VITE_HOST_WEB"] . ":" . $_ENV["VITE_PORT"],
+$vite = new \Ponponumi\ViteLoader\ViteLoader(__DIR__ . "/build/.vite/manifest.json", $projectRootUrl . "/build", false, [
+    "devMode" => true,
+    // "devHost" => $_ENV["VITE_HOST"] . ":" . $_ENV["VITE_PORT"],
+    // "devHostWeb" => $_ENV["VITE_HOST_WEB"] . ":" . $_ENV["VITE_PORT"],
 ]);
 // $vite = new \Ponponumi\ViteLoader\ViteLoader(__DIR__ . "/build/.vite/manifest.json",$projectRootUrl . "/build");
 
@@ -19,55 +19,58 @@ $vite = new \Ponponumi\ViteLoader\ViteLoader(__DIR__ . "/build/.vite/manifest.js
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>テスト</title>
-  <?php $vite->html("asset/scss/style.scss"); ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト</title>
+    <?php $vite->html("asset/scss/style.scss"); ?>
 </head>
+
 <body>
-  <h1>テスト</h1>
-  <p>テキストテキスト</p>
+    <h1>テスト</h1>
+    <p>テキストテキスト</p>
 
-  <ul>
-    <li>テキスト</li>
-    <li>テキスト</li>
-    <li class="test">テキスト</li>
-  </ul>
+    <ul>
+        <li>テキスト</li>
+        <li>テキスト</li>
+        <li class="test">テキスト</li>
+    </ul>
 
-  <pre>
+    <pre>
     <?php var_dump($vite->manifestDataGet()); ?>
   </pre>
-  <?php $vite->htmlList([
-    "asset/js/script.js",
-    "asset/ts/script.ts",
-    "asset/ts/test.ts",
-    "asset/scss/style.scss",
-  ]); ?>
-  <p>ホスト: <?= htmlspecialchars($_ENV["VITE_HOST"] . ":" . $_ENV["VITE_PORT"]) ?></p>
+    <?php $vite->htmlList([
+        "asset/js/script.js",
+        "asset/ts/script.ts",
+        "asset/ts/test.ts",
+        "asset/scss/style.scss",
+    ]); ?>
+    <p>ホスト: <?= htmlspecialchars($_ENV["VITE_HOST"] . ":" . $_ENV["VITE_PORT"]) ?></p>
 
-  <pre>
+    <pre>
     <?php var_dump($vite->separateByTypeWebPathListGet([
-      "asset/js/script.js",
-      "asset/ts/script.ts",
-      "asset/ts/test.ts",
-      "asset/scss/style.scss",
+        "asset/js/script.js",
+        "asset/ts/script.ts",
+        "asset/ts/test.ts",
+        "asset/scss/style.scss",
     ])); ?>
   </pre>
 
-  <p><?= $vite->typeGetExtension("TS") ?></p>
-  <p><?= $vite->typeGetExtension("js") ?></p>
-  <p><?= $vite->typeGetExtension("css") ?></p>
-  <p><?= $vite->typeGetExtension("scss") ?></p>
+    <p><?= $vite->typeGetExtension("TS") ?></p>
+    <p><?= $vite->typeGetExtension("js") ?></p>
+    <p><?= $vite->typeGetExtension("css") ?></p>
+    <p><?= $vite->typeGetExtension("scss") ?></p>
 
-  <?php
+    <?php
 
-  $webPath = $vite->buildWebPathGet("asset/scss/style.scss");
-  $sourcePath = $vite->sourcePathGet($webPath);
+    $webPath = $vite->buildWebPathGet("asset/scss/style.scss");
+    $sourcePath = $vite->sourcePathGet($webPath);
 
-  ?>
+    ?>
 
-  <p>Webのパス: <?= $webPath ?></p>
-  <p>ソースのパス: <?= $sourcePath ?></p>
+    <p>Webのパス: <?= $webPath ?></p>
+    <p>ソースのパス: <?= $sourcePath ?></p>
 </body>
+
 </html>
