e73ca21bf483795bc35cf68476a3e7655b9c2136
ponponumi <ponponumi@gmail.com>
2024-12-24 12:39:03 +0900

    拡張子からタイプを取得するメソッドを作成

--- src/TypeGet.php
+++ src/TypeGet.php
@@ -4,5 +4,29 @@ namespace Ponponumi\ViteLoader;
 
 class TypeGet
 {
-    //
+    public static function extension(string $extension): string
+    {
+        $type = "";
+
+        switch (mb_strtolower($extension)) {
+            case "css":
+            case "scss":
+            case "sass":
+            case "less":
+            case "stylus":
+            case "styl":
+                $type = "style";
+                break;
+
+            case "js":
+            case "ts":
+            case "jsx":
+            case "tsx":
+            case "coffee":
+                $type = "script";
+                break;
+        }
+
+        return $type;
+    }
 }
