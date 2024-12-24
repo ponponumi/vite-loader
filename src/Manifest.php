266b6e7dd6929dffbdc440b22ac1acd7a6b2f778
ponponumi <ponponumi@gmail.com>
2024-12-24 12:24:59 +0900

    manifest.jsonを読み込むメソッドを作成

--- src/Manifest.php
+++ src/Manifest.php
@@ -4,5 +4,18 @@ namespace Ponponumi\ViteLoader;
 
 class Manifest
 {
-    //
+    public static function load(string $path): array
+    {
+        // manifest.jsonを読み込む
+        if (file_exists($path)) {
+            $json = file_get_contents($path);
+            $data = json_decode($json, true);
+
+            if($data !== NULL){
+                return $data;
+            }
+        }
+
+        return [];
+    }
 }
