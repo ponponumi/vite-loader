904ad0c10b740925acdb947196df5ddde2b6e9eb
ponponumi <ponponumi@gmail.com>
2024-12-24 12:11:44 +0900

    JSのリンクを作るメソッドを作成

--- src/LinkCreate.php
+++ src/LinkCreate.php
@@ -9,4 +9,15 @@ class LinkCreate
         // CSSのリンクを作る
         return '<link rel="stylesheet" href="' . $url . '">';
     }
+
+    public static function jsLinkCreate(string $url, $moduleMode = false) {
+        // JSのリンクを作る
+        $module = "";
+
+        if ($moduleMode) {
+            $module = 'type="module" ';
+        }
+
+        return '<script ' . $module . 'src="' . $url . '"></script>';
+    }
 }
