21fb1b60c33f5c55f1a5985573bb18fb66e4c850
ponponumi <ponponumi@gmail.com>
2024-12-24 12:10:47 +0900

    CSSのリンクを作るメソッドを作成

--- src/LinkCreate.php
+++ src/LinkCreate.php
@@ -4,5 +4,9 @@ namespace Ponponumi\ViteLoader;
 
 class LinkCreate
 {
-    //
+    public static function cssLinkCreate(string $url): string
+    {
+        // CSSのリンクを作る
+        return '<link rel="stylesheet" href="' . $url . '">';
+    }
 }
