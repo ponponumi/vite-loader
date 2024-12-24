581b2e0928c87c3edd171aab88702c8c195754a9
ponponumi <ponponumi@gmail.com>
2024-12-24 12:26:10 +0900

    型を指定

--- src/ViteLoader.php
+++ src/ViteLoader.php
@@ -14,7 +14,7 @@ class ViteLoader {
     public string $viteReloadPath = "";
     public $moduleMode = false;
 
-    public function __construct($manifestPath, $buildPath = "", $errorMode = false, array $viteDevServer = [], $moduleMode = false) {
+    public function __construct(string $manifestPath, string $buildPath = "", $errorMode = false, array $viteDevServer = [], $moduleMode = false) {
         $this->manifestPath = $manifestPath;
         $this->buildPath = $this->lastSlashAdd($buildPath);
 
