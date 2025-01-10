18a952ee08c07f11d6635c8866433e29a605e9fb
ponponumi <ponponumi@ponponumi.com>
2025-01-11 00:40:59 +0900

    パスにスラッシュが追加されない問題を確認

--- test/ManifestTest.php
+++ test/ManifestTest.php
@@ -12,3 +12,10 @@ var_dump($manifest);
 
 $path = \Ponponumi\ViteLoader\Manifest::dataGet("asset/js/script.js", $manifest);
 var_dump($path);
+
+$webPath = \Ponponumi\ViteLoader\Manifest::webPathGet(
+    "asset/js/script.js",
+    $manifest,
+    "http://localhost"
+);
+var_dump($webPath);
