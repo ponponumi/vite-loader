10284da23bdf312c8e4058ad310240e6e3f76a88
ponponumi <ponponumi@ponponumi.com>
2025-01-11 00:35:56 +0900

    空の配列が取得されることを確認

--- test/ManifestTest.php
+++ test/ManifestTest.php
@@ -5,3 +5,7 @@ require __DIR__ . "/../vendor/autoload.php";
 // 正しいパス
 $manifest = \Ponponumi\ViteLoader\Manifest::load(__DIR__ . "/build/.vite/manifest.json");
 var_dump($manifest);
+
+// 間違ったパス
+$manifest = \Ponponumi\ViteLoader\Manifest::load(__DIR__ . "/build/vite/manifest.json");
+var_dump($manifest);
