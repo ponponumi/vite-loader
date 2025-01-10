69fd551256f088a64342e9016a8be5db599b1ef4
ponponumi <ponponumi@ponponumi.com>
2025-01-11 00:39:01 +0900

    データが取得されることを確認

--- test/ManifestTest.php
+++ test/ManifestTest.php
@@ -9,3 +9,6 @@ var_dump($manifest);
 // 正しいパス
 $manifest = \Ponponumi\ViteLoader\Manifest::load(__DIR__ . "/build/.vite/manifest.json");
 var_dump($manifest);
+
+$path = \Ponponumi\ViteLoader\Manifest::dataGet("asset/js/script.js", $manifest);
+var_dump($path);
