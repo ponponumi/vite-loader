020446dfd00f7de4c417e6f45e203c466e6c8acc
ponponumi <ponponumi@gmail.com>
2025-01-15 12:43:41 +0900

    ビルド済みデータの取得テストをするファイルを作成

---
+++ test/buildMainTest.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$projectRootPath = __DIR__;
+$projectRootUrl = (!empty($_SERVER['HTTPS']) ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . str_replace($_SERVER['DOCUMENT_ROOT'], '', $projectRootPath);
+
+$vite = new \Ponponumi\ViteLoader\ViteLoader(__DIR__ . "/build/.vite/manifest.json", $projectRootUrl . "/build");
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>テスト</title>
+</head>
+<body>
+    <!--  -->
+</body>
+</html>
